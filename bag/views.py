3cb136dbd4a9eb3cdc38d686b84997b02e427afb
R.N <148905439+RNXVIII@users.noreply.github.com>
2024-12-19 04:34:47 +0000

    added more on context Processer and also styled bag.html for desktop

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
-
 from django.shortcuts import render, redirect
+
+# Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -19,6 +20,5 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
     
